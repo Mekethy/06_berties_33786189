b953624d247049c5624f17aff8e3db2d9d40f9d1
Kenny Hoang <kennyhoang1404@gmail.com>
2025-11-13 16:54:03 +0000

    final draft

--- routes/books.js
+++ routes/books.js
@@ -6,21 +6,74 @@ router.get('/search',function(req, res, next){
     res.render("search.ejs")
 });
 
+// Lists results from search
 router.get('/search-result', function (req, res, next) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+    let keyword = req.query.search_text;   
+
+// Create SQL query (? to prevent sql injection)
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+    let searchTerm = '%' + keyword + '%';
+
+// Query the database
+    db.query(sqlquery, [searchTerm], (err, result) => {
+        if (err) {
+            return next(err);
+        }
+
+// Render + pass results
+        res.render("search-result", {
+            searchTerm: keyword,
+            foundBooks: result
+        });
+    });
 });
 
+
+router.get('/addbook', function(req, res, next) {
+    res.render('addbook.ejs');
+});
+
+
+// LIST ALL BOOKS
 router.get('/list', function(req, res, next) {
     let sqlquery = "SELECT * FROM books"; // query database to get all the books
-    // execute sql query
     db.query(sqlquery, (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        
+        // Render list.ejs and pass the result
+        res.render("list.ejs", { availableBooks: result })
+    });
+});
+
+router.get('/bargainbooks', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books WHERE price < 20";
+
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            return next(err);
+        }
+
+        res.render("bargainbooks.ejs", { cheapBooks: result });
+    });
+});
+
+
+router.post('/bookadded', function (req, res, next) {
+    // saving data in database
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+    // execute sql query
+    let newrecord = [req.body.name, req.body.price]
+    db.query(sqlquery, newrecord, (err, result) => {
         if (err) {
             next(err)
         }
-        res.send(result)
-        });
-});
+        else
+            res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price)
+    })
+}) 
+
 
 // Export the router object so index.js can access it
 module.exports = router
